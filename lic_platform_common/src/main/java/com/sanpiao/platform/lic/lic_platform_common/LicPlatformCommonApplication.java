704f07898d063ff7e0f69804236a502243c9d280
lichuan <lichuan@e-lead.cn>
2018-08-19 09:03:03 +0800

    add  common

---
+++ lic_platform_common/src/main/java/com/sanpiao/platform/lic/lic_platform_common/LicPlatformCommonApplication.java
@@ -0,0 +1,12 @@
+package com.sanpiao.platform.lic.lic_platform_common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicPlatformCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicPlatformCommonApplication.class, args);
+	}
+}
---
+++ lic_platform_common/src/test/java/com/sanpiao/platform/lic/lic_platform_common/LicPlatformCommonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sanpiao.platform.lic.lic_platform_common;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LicPlatformCommonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
